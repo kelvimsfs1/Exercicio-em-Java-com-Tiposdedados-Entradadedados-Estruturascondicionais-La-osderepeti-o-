76613742bc713c217ac86f2938b5e8ff37d3f2a4
kelvimsfs1 <49540418+kelvimsfs1@users.noreply.github.com>
2019-06-16 22:57:39 -0300

    Create Classe Pessoa.java

---
+++ Classe Pessoa.java
@@ -0,0 +1,120 @@
+'''package trab;
+
+import java.sql.Date;
+
+public class Pessoa {
+
+	public static void main(String[] args) {
+		
+	    }
+		
+		private String enderecoEmail;
+
+		private String nome;
+
+		private String sobreNome;
+
+		private String senha;
+
+		private Date dataNascimento;
+		
+		private Reserva reserva;
+		private Imovel  imovel;
+		
+		public Reserva getReserva() {
+			return reserva;
+		}
+
+		public void setReserva(Reserva reserva) {
+			this.reserva = reserva;
+		}
+
+		public Imovel getImovel() {
+			return imovel;
+		}
+
+		public void setImovel(Imovel imovel) {
+			this.imovel = imovel;
+		}
+
+		public void cadastrarImovel() {
+
+		}
+
+		public void cadastrarUsuario() {
+
+		}
+
+		public void alugarImovel() {
+
+		}
+
+		public void alterarImovel() {
+
+		}
+
+		public void alterarUsuario() {
+
+		}
+
+		public void consultarImovel() {
+
+		}
+
+		public void consultarUsuario() {
+
+		}
+
+		public void excluirImovel() {
+
+		}
+
+		public void excluirUsuario() {
+
+		}
+
+		public void setEnderecoEmail(String enderecoEmail) {
+	        this.enderecoEmail = enderecoEmail;
+		}
+
+		public String getEnderecoEmail() {
+			return enderecoEmail;
+		}
+
+		public void setNome(String nome) {
+	        this.nome = nome;
+		}
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setSobreNome( String sobreNome) {
+	        this.sobreNome = sobreNome;
+		}
+
+		public String getSobreNome() {
+			return sobreNome;
+		}
+
+		public void setSenha(String senha) {
+	        this.senha = senha;
+		}
+
+		public String getSenha() {
+			return senha;
+		}
+
+		public void setDataNascimento(Date dataNascimento) {
+	        this.dataNascimento = dataNascimento;
+		}
+
+		public Date getDataNascimento() {
+			return dataNascimento;
+		}
+
+	 {
+		 
+	 }
+
+}'''
